893b023c5dc2680326c279c52976c087cf34859b
Hamza Haroon <95119705+hamzaharooon@users.noreply.github.com>
2022-08-15 14:27:47 +0500

    Add files via upload

---
+++ Problem Set 4/3. Adieu, Adieu/adieu.py
@@ -0,0 +1,17 @@
+import inflect
+
+p=inflect.engine()
+
+names = [ ]
+text = "Adieu, adieu, to "
+
+while True:
+        try:
+            name = input("Name: ")
+            names.append(name)
+        except EOFError:
+            print()
+            break
+
+join_names = p.join(names)
+print(text + join_names)
